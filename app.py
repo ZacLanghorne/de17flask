0ac502161752d3b2b3155ea33510056035e7efc8
Zac Langhorne <langhornez@yahoo.co.uk>
2020-09-29 12:10:21 +0100

    Added multiple pages; consultant, client.

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return 'Welcome to my AWESOME webpage!'
+
+@app.route('/consultant/<string:cohort>/<string:consultant>')
+def consultant(cohort: str,consultant: str):
+    return f'This is the consultant portal for {consultant} in cohort: {cohort}.'
+
+@app.route('/client')
+def client():
+    return 'This is the client portal.'
 
 
 if __name__ == '__main__':
